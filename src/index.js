ed1f286e67559b5cb67c9a2ed7a87e49a2ba631b
Jon Bellah <jon@ted.com>
2019-01-29 14:58:02 -0700

    Imports

--- src/index.js
+++ src/index.js
@@ -2,23 +2,14 @@
 import Button from './Button';
 import Container from './Container';
 import DataTable from './DataTable';
+import Dropdown from './Dropdown';
 import Input from './Input';
 import Navbar from './Navbar';
-import Select from './Select';
-import Dropdown from './Dropdown';
-import TwistDown from './TwistDown';
 import NavFlaps from './NavFlaps';
 import Tab from './NavFlaps/Tab';
+import NavToggle from './NavToggle';
+import Select from './Select';
+import TwistDown from './TwistDown';
 
-export {
-  Button,
-  Container,
-  DataTable,
-  Input,
-  Navbar,
-  Select,
-  Dropdown,
-  TwistDown,
-  NavFlaps,
-  Tab,
-};
+export { Button, Container, DataTable, Input, Navbar, Select, Dropdown, TwistDown, NavFlaps, Tab, NavToggle, };
+
